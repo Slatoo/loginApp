6bc183f483180eff9d33b55108e299399be2ad90
Slatoo <josemar7jam@gmail.com>
2022-04-25 04:02:02 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <section className="App-content">
+        <h1>HOLA</h1>
+      </section>
+    </div>
+  );
+}
